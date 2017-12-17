3ca46635ed0c03311ba57d1446caba396a814c2b
Balintataw <Balintataw2@gmail.com>
2017-12-17 12:59:30 -0800

    added company logo clickability

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,10 +1,15 @@
-var video = document.getElementById("video_1");
+/*adds/removes video controls bar*/
 
-$(video).on({
-    mouseenter: function () {
-      video.setAttribute("controls","controls")
-    },
-    mouseleave: function () {
-      video.removeAttribute("controls");
-    }
+$(document).ready(function() {
+  
+  var video = document.getElementById("video_1");
+
+  $(video).on({
+      mouseenter: function () {
+        video.setAttribute("controls","controls")
+      },
+      mouseleave: function () {
+        video.removeAttribute("controls");
+      }
+  });
 });
